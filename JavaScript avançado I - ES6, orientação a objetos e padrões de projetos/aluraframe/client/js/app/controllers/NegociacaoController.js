6710172942a6cb450a16f459064a11a57e0f46ea
José Carlos de Freitas <Jos Carlos de Freitas>
2017-02-23 11:14:19 -0300

    finalizei curso de js avançado e outros

--- JavaScript avançado I - ES6, orientação a objetos e padrões de projetos/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ JavaScript avançado I - ES6, orientação a objetos e padrões de projetos/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -4,10 +4,16 @@ class NegociacaoController{
 
 		let $ = document.querySelector.bind(document);
 
-		this._inputData = $("#data");
-		this._inputQuantidade = $("#quantidade");
-		this._inputValor = $("#valor");
+		this._inputData = $('#data');
+		this._inputQuantidade = $('#quantidade');
+		this._inputValor = $('#valor');
 		this._listaNegociacoes = new ListaNegociacoes();
+		this._negociacoesView = new NegociacoesView($('#negociacoes'));
+		this._negociacoesView.update(this._listaNegociacoes);
+
+		this._mensagem = new Mensagem();
+		this._mensagemView = new MensagemView($('#mensagemView'));
+		this._mensagemView.update(this._mensagem);
 	}
 
 	adiciona(event){
@@ -15,6 +21,11 @@ class NegociacaoController{
 		event.preventDefault();
 		
 		this._listaNegociacoes.adiciona(this._criaNegociacao());
+		this._negociacoesView.update(this._listaNegociacoes);
+
+		this._mensagem.texto = 'Negociação adicionada com sucesso!';
+		this._mensagemView.update(this._mensagem);
+
 		this._resetaForm();
 	}
 
