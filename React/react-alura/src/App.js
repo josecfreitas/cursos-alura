6fc69b690275085bd2fa6d2a74dbfc8d9ec794b1
José Carlos de Freitas <jcfreitas2000@hotmail.com>
2020-05-06 18:25:25 -0300

    React

---
+++ React/react-alura/src/App.js
@@ -0,0 +1,66 @@
+import React, {Component, Fragment} from 'react';
+import './App.css';
+import 'materialize-css/dist/css/materialize.min.css'
+import Tabela from "./Tabela";
+import Formulario from "./Formulario";
+import Header from "./Header";
+
+class App extends Component {
+
+    state = {
+        autores: [
+            {
+                nome: 'Paulo',
+                livro: 'React',
+                preco: '1000'
+            },
+            {
+                nome: 'Daniel',
+                livro: 'Java',
+                preco: '99'
+            },
+            {
+                nome: 'Marcos',
+                livro: 'Design',
+                preco: '150'
+            },
+            {
+                nome: 'Bruno',
+                livro: 'DevOps',
+                preco: '100'
+            }
+        ]
+    };
+
+    removeAutor = index => {
+
+        const {autores} = this.state;
+
+        this.setState({
+            autores: autores.filter((autor, i) => {
+                return i !== index;
+            }),
+        });
+
+    };
+
+    formSubmit = autor => {
+        console.log(autor);
+        this.setState({autores: [...this.state.autores, autor]});
+    };
+
+    render() {
+        return (
+            <Fragment>
+                <Header/>
+                <div className="container mb-10">
+                    <h1>Casa do código</h1>
+                    <Tabela autores={this.state.autores} removeAutor={this.removeAutor}/>
+                    <Formulario submit={this.formSubmit}/>
+                </div>
+            </Fragment>
+        );
+    }
+}
+
+export default App;
